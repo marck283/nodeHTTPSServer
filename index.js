d8a12bcc0f94c3ea90fc7692cc2507dee7720773
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-05-05 09:25:57 +0200

    Secondo commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var express = require('express');
 var app = express();
 var https = require('https');
+var port = 8000;
+
+app.get("/", (req, res) => {
+    console.log("GET request received on port " + port);
+});
+
+const options = {
+    key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+    cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+};
+
+https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+});
