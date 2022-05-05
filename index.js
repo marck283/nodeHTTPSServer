b9b255871a7540230e8448bf7f94b5bfd0ee462a
marck283 <86911602+marck283@users.noreply.github.com>
2022-05-05 09:45:19 +0200

    Added MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var https = require('https');
 const fs = require("fs");
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://marck28:Travel012095@cluster0.nf2l6.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 var app = express();
 var port = 8000;
@@ -8,6 +11,19 @@ var port = 8000;
 app.get("/", (req, res) => {
     console.log("GET request received on port " + port);
 });
+
+async function run() {
+    try {
+      // Connect the client to the server
+      await client.connect();
+      // Establish and verify connection
+      await client.db("marck283").command({ ping: 1 });
+      console.log("Connected successfully to server");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+}
 
 const options = {
     key: fs.readFileSync('server-key.pem'),
