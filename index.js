59628beb5e95cea3e56d68ea33f70c7f3c79d023
marck283 <86911602+marck283@users.noreply.github.com>
2022-05-05 10:21:08 +0200

    Edited connection code lines

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var https = require('https');
 const fs = require("fs");
-const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://marck28:Travel012095@cluster0.nf2l6.mongodb.net/Cluster0?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://marck28:<password>@cluster0.nf2l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 var app = express();
 var port = 8000;
@@ -11,19 +16,6 @@ var port = 8000;
 app.get("/", (req, res) => {
     console.log("GET request received on port " + port);
 });
-
-async function run() {
-    try {
-      // Connect the client to the server
-      await client.connect();
-      // Establish and verify connection
-      await client.db("marck283").command({ ping: 1 });
-      console.log("Connected successfully to server");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-}
 
 const options = {
     key: fs.readFileSync('server-key.pem'),
